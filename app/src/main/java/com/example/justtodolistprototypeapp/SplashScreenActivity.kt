17390c79e889929b42a1a964635e9d8b0e0d44d4
Nurislam <gggrrraaa1235@gmail.com>
2024-02-17 23:48:28 +0600

    7th commit connecting binding for SplashScreenActivity

--- app/src/main/java/com/example/justtodolistprototypeapp/SplashScreenActivity.kt
+++ app/src/main/java/com/example/justtodolistprototypeapp/SplashScreenActivity.kt
@@ -1,11 +1,17 @@
 package com.example.justtodolistprototypeapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.justtodolistprototypeapp.databinding.ActivitySplashScreenBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
