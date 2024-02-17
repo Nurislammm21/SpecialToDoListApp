ec9bdc63c2ce937ac865b166f14eff0e6b3bd7ed
Nurislam <gggrrraaa1235@gmail.com>
2024-02-17 13:56:45 +0600

    second commit create a data class

---
+++ app/src/main/java/com/example/justtodolistprototypeapp/domain/data/ToDoItem.kt
@@ -0,0 +1,6 @@
+package com.example.justtodolistprototypeapp.domain.data
+
+data class ToDoItem(val id: Int = 0,
+                    val title: String,
+                    val description: String
+    )
