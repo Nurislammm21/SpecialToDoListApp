f96403929226585b9feaea07158bd26f8a0158fa
Nurislam <gggrrraaa1235@gmail.com>
2024-02-18 00:20:09 +0600

    14th commit creating HiltApplication class and adding in manifest

---
+++ app/src/main/java/com/example/justtodolistprototypeapp/HiltApplication.kt
@@ -0,0 +1,10 @@
+package com.example.justtodolistprototypeapp
+
+import android.app.Application
+
+class HiltApplication: Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
