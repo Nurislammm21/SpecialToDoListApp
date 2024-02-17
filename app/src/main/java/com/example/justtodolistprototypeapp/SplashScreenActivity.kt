4235aaa57154ac9dcbab1228903855dd9d633a2f
Nurislam <gggrrraaa1235@gmail.com>
2024-02-18 00:04:33 +0600

    10th commit adding and creating countdowntimer in splashScreenActivity for working to connect to use

--- app/src/main/java/com/example/justtodolistprototypeapp/SplashScreenActivity.kt
+++ app/src/main/java/com/example/justtodolistprototypeapp/SplashScreenActivity.kt
@@ -1,17 +1,37 @@
 package com.example.justtodolistprototypeapp
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.example.justtodolistprototypeapp.databinding.ActivitySplashScreenBinding
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashScreenBinding
+    private lateinit var timer: CountDownTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        timer = object : CountDownTimer(2500,1000){
+            override fun onTick(p0: Long) {
+
+            }
+
+            override fun onFinish() {
+                startActivity(Intent(this@SplashScreenActivity,MainActivity::class.java))
+            }
+
+        }.start()
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.cancel()
     }
 }
