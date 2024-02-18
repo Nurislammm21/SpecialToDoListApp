f272c96a374d5ee9155efd70f0b2d78bc3140371
Nurislam <gggrrraaa1235@gmail.com>
2024-02-18 18:42:47 +0600

    42th commit added fun  to retrieve ids follow otc

--- app/src/main/java/com/example/justtodolistprototypeapp/UI/EditToDoListFragment.kt
+++ app/src/main/java/com/example/justtodolistprototypeapp/UI/EditToDoListFragment.kt
@@ -6,52 +6,72 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.justtodolistprototypeapp.R
 import com.example.justtodolistprototypeapp.databinding.FragmentEditToDoListBinding
 import com.example.justtodolistprototypeapp.domain.models.ToDoEntity
+import kotlinx.coroutines.launch
 
 
 class EditToDoListFragment : Fragment() {
-    private lateinit var binding : FragmentEditToDoListBinding
-    private val viewModel : ToDoListVIewModel by viewModels()
-    lateinit var todo : ToDoEntity
+    private lateinit var binding: FragmentEditToDoListBinding
+    private val viewModel: ToDoListVIewModel by viewModels()
+    lateinit var todo: ToDoEntity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View{
-        binding = FragmentEditToDoListBinding.inflate(inflater,container,false)
+    ): View {
+        binding = FragmentEditToDoListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initLogic()
+        val id = requireArguments().getInt("id")
+        viewModel.retrieveToDo(id)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.todo.collect { selectedTodo ->
+                selectedTodo?.let {
+                    todo = selectedTodo
+                    bind(todo)
+                }
+            }
+        }
     }
 
-    private fun initLogic() = with(binding){
-        binding.toolbarEditTodo.deleteClick = {
+    private fun initLogic() = with(binding) {
+        toolbarEditTodo.deleteClick = {
             if (todoIsInitialized()) {
                 viewModel.deleteToDo(todo)
             }
         }
-        binding.toolbarEditTodo.saveClick = { saveEntry() }
+        toolbarEditTodo.saveClick = { saveEntry() }
     }
 
-    private fun saveEntry() = with(binding){
-        val todoTitle = todoTitle.text.toString()
-        val todoDescription = todoDescription.text.toString()
-
-        if(viewModel.isEntryValid(todoTitle,todoDescription)){
-            if(todoIsInitialized()){
-                viewModel.updateToDO(todo.id,todoTitle,todoDescription)
-            } else {
-                viewModel.addNewToDo(todoTitle,todoDescription)
-            }
+    private fun bind(todo: ToDoEntity) {
+        binding.apply {
+            todoTitle.setText(todo.title)
+            todoDescription.setText(todo.description)
         }
     }
 
+        private fun saveEntry() = with(binding) {
+            val todoTitle = todoTitle.text.toString()
+            val todoDescription = todoDescription.text.toString()
+
+            if (viewModel.isEntryValid(todoTitle, todoDescription)) {
+                if (todoIsInitialized()) {
+                    viewModel.updateToDO(todo.id, todoTitle, todoDescription)
+                } else {
+                    viewModel.addNewToDo(todoTitle, todoDescription)
+                }
+            }
+        }
 
     private fun todoIsInitialized() = ::todo.isInitialized
+}
 
-    }
+
