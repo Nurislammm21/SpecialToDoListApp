a1cd8d78749d9e89ad30a7dd2b1e119aa5ea29ed
Nurislam <gggrrraaa1235@gmail.com>
2024-02-18 16:41:53 +0600

    35th commit added MediatorLiveData for viewModel to convert data

--- app/src/main/java/com/example/justtodolistprototypeapp/UI/ToDoListVIewModel.kt
+++ app/src/main/java/com/example/justtodolistprototypeapp/UI/ToDoListVIewModel.kt
@@ -1,4 +1,33 @@
 package com.example.justtodolistprototypeapp.UI
 
-class ToDoListVIewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.ViewModel
+import com.example.justtodolistprototypeapp.domain.data.ToDoItem
+import com.example.justtodolistprototypeapp.domain.models.ToDoEntity
+import com.example.justtodolistprototypeapp.domain.repository.ToDoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import javax.inject.Inject
+
+class ToDoListVIewModel @Inject constructor(private val toDoRepository: ToDoRepository): ViewModel() {
+    val todoS : Flow<List<ToDoEntity>> = toDoRepository.getToDOs()
+    val todo = MutableStateFlow<ToDoEntity?>(null)
+
+    private val allToDo: LiveData<List<ToDoEntity>> = toDoRepository.getAllToDoAsc()
+
+    private val _todos = MediatorLiveData<List<ToDoItem>>().apply {
+        addSource(allToDo){ todoNur ->
+            val todoItems = todoNur.map {todo ->
+                ToDoItem(todo.id,todo.title,todo.description)
+
+            }
+            postValue(todoItems)
+
+
+        }
+    }
+
+    val todos: LiveData<List<ToDoItem>> = _todos
+
 }
