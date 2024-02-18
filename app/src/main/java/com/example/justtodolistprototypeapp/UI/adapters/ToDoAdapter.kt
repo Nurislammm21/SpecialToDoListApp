66a62feaccfd0246ee97a8df8237f535fc601292
Nurislam <gggrrraaa1235@gmail.com>
2024-02-18 16:19:20 +0600

    33rd commit completely finished features of adapter for FragmentMainList

--- app/src/main/java/com/example/justtodolistprototypeapp/UI/adapters/ToDoAdapter.kt
+++ app/src/main/java/com/example/justtodolistprototypeapp/UI/adapters/ToDoAdapter.kt
@@ -1,4 +1,47 @@
 package com.example.justtodolistprototypeapp.UI.adapters
 
-class ToDoAdapter  {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.justtodolistprototypeapp.databinding.TodoListItemBinding
+import com.example.justtodolistprototypeapp.domain.models.ToDoEntity
+
+class ToDoAdapter(private val onItemClicked: (ToDoEntity) -> Unit): ListAdapter<ToDoEntity,ToDoAdapter.ToDoViewHolder>(
+    DIFF_CALLBACK)   {
+
+    class ToDoViewHolder(private val binding: TodoListItemBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(todo: ToDoEntity) = with(binding){
+            titleItem.text = todo.title
+            descItem.text = todo.description
+
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
+        return ToDoViewHolder(TodoListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
+        val todo = currentList[position]
+        holder.bind(todo)
+        holder.itemView.setOnClickListener{
+            onItemClicked(todo)
+        }
+    }
+
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<ToDoEntity>() {
+            override fun areItemsTheSame(oldItem: ToDoEntity, newItem: ToDoEntity): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: ToDoEntity, newItem: ToDoEntity): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
 }
