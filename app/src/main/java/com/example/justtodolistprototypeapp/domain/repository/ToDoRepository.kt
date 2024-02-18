226383b556fad40da39a47e7532c439e6124c6f6
Nurislam <gggrrraaa1235@gmail.com>
2024-02-18 14:51:15 +0600

    30th commit created ToDoRepositoryImpl  class for implementing

--- app/src/main/java/com/example/justtodolistprototypeapp/domain/repository/ToDoRepository.kt
+++ app/src/main/java/com/example/justtodolistprototypeapp/domain/repository/ToDoRepository.kt
@@ -1,8 +1,12 @@
 package com.example.justtodolistprototypeapp.domain.repository
 
 import androidx.lifecycle.LiveData
+import com.example.justtodolistprototypeapp.domain.models.ToDoDao
 import com.example.justtodolistprototypeapp.domain.models.ToDoEntity
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
 interface ToDoRepository {
         suspend fun insert(todo : ToDoEntity)
@@ -17,5 +21,35 @@ interface ToDoRepository {
 
         suspend fun deleteAll()
 
+
+}
+
+
+class ToDoRepositoryImpl @Inject constructor(private val toDoDao: ToDoDao) : ToDoRepository{
+        override suspend fun insert(todo: ToDoEntity) = withContext(Dispatchers.IO) {
+                        toDoDao.insert(todo)
+        }
+
+        override suspend fun update(todo: ToDoEntity) = withContext(Dispatchers.IO) {
+                toDoDao.update(todo)
+        }
+
+        override suspend fun delete(todo: ToDoEntity) = withContext(Dispatchers.IO) {
+              toDoDao.delete(todo)
+        }
+
+        override suspend fun getToDo(id: Int): Flow<ToDoEntity> = withContext(Dispatchers.IO) {
+                toDoDao.getToDO(id)
+        }
+
+        override fun getToDOs(): Flow<List<ToDoEntity>> = toDoDao.getToDOs()
+
+        override fun getAllToDoAsc(): LiveData<List<ToDoEntity>> {
+               return toDoDao.getAllToDoAsc()
+        }
+
+        override suspend fun deleteAll() {
+                toDoDao.deleteAll()
+        }
 
 }
