5bdbc9cff3e957f80bfa7a5d3fbfa192d01b99a8
Nurislam <gggrrraaa1235@gmail.com>
2024-02-18 14:53:04 +0600

    31st commit created RepositoryModule object for Repository of project for special cases like a unit testing

---
+++ app/src/main/java/com/example/justtodolistprototypeapp/domain/repository/RepositoryModule.kt
@@ -0,0 +1,18 @@
+package com.example.justtodolistprototypeapp.domain.repository
+
+import com.example.justtodolistprototypeapp.domain.models.ToDoDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+
+@InstallIn(ViewModelComponent::class)
+@Module
+object RepositoryModule {
+
+
+    @Provides
+    fun provideTodoRepository(toDoDao: ToDoDao): ToDoRepository = ToDoRepositoryImpl(toDoDao)
+
+}
