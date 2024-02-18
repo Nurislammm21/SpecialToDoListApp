19b7bf6f2f386b410393fdaac14e69326122cafe
Nurislam <gggrrraaa1235@gmail.com>
2024-02-18 17:25:04 +0600

    40th commit added added fun for floating btn and for searchView onQueryTextListener almost done with main Fragment class

--- app/src/main/java/com/example/justtodolistprototypeapp/UI/ToDoListFragmentMain.kt
+++ app/src/main/java/com/example/justtodolistprototypeapp/UI/ToDoListFragmentMain.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -16,7 +17,7 @@ import com.example.justtodolistprototypeapp.databinding.FragmentToDoListMainBind
 import kotlinx.coroutines.launch
 
 
-class ToDoListFragmentMain : Fragment() {
+class ToDoListFragmentMain : Fragment(), SearchView.OnQueryTextListener {
     private lateinit var binding: FragmentToDoListMainBinding
     private val viewModel : ToDoListVIewModel by viewModels()
 
@@ -30,7 +31,12 @@ class ToDoListFragmentMain : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        initRcView()
+        fabClick()
+        binding.searchView.setOnQueryTextListener(this)
+        binding.deleteAllBtn.setOnClickListener {
+            viewModel.clear()
+        }
     }
 
 
@@ -50,6 +56,23 @@ class ToDoListFragmentMain : Fragment() {
         rcView.adapter = adapter
         rcView.layoutManager = StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.VERTICAL)
         rcView.setHasFixedSize(true)
+    }
+
+    private fun fabClick(){
+        binding.floatingActionButton.setOnClickListener{
+            val action = ToDoListFragmentMainDirections.actionToDoListFragmentMainToEditToDoListFragment()
+            findNavController().navigate(action)
+        }
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        viewModel.onSearchQueryChanged(query)
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        viewModel.onSearchQueryChanged(newText)
+        return true
     }
 
 }
